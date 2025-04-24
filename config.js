5cfaaa5e3878e9aaaa2f3d8d5589c4953267afe6
DanielChristopherHamby <hambydc@dukes.jmu.edu>
2025-04-24 16:46:31 -0400

    keezell

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Someplace Georgia',
-            image: '/assets/firestone_farm.jpg',
+            image: '/assets/keezell_hall.jpg',
             description: 'Description of the second location.',
             location: {
                 center: [-83.5573651, 32.5573651], 
