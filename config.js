964ccbc658b38057e32b380e12245e3329b86ddc
DanielChristopherHamby <hambydc@dukes.jmu.edu>
2025-04-24 16:29:44 -0400

    Keezell Hall

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Someplace Georgia',
-            image: '/assets/Metal_Gear_cover.jpg',
+            image: '/assets/Keezell_Hall.jpg',
             description: 'Description of the second location.',
             location: {
                 center: [-83.5573651, 32.5573651], 
